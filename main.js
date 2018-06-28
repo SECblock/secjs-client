abf3aabb13a6d7be732a3e42229c8ab6979fdc2c
theFay <fei.minhao@gmail.com>
2018-06-29 00:00:08 +0200

    make some change

---
+++ main.js
@@ -0,0 +1,14 @@
+const {app, BrowserWindow} = require('electron')
+
+let win
+
+function createWindow () {
+  win = new BrowserWindow({width: 800, height: 600})
+  win.loadFile('index.html')
+
+  // win.close('closed', () => {
+  //   win = null
+  // })
+}
+
+app.on('ready', createWindow)
