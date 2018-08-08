420a093f3ac3c14e22ce5746d07625e18a574a34
theFay <fei.minhao@gmail.com>
2018-08-08 19:20:00 +0200

    create express project

--- main.js
+++ main.js
@@ -1,14 +1,97 @@
-const {app, BrowserWindow} = require('electron')
-
+const {app, BrowserWindow, Menu} = require('electron')
+const path = require('path')
 let win
 
 function createWindow () {
   win = new BrowserWindow({width: 800, height: 600})
-  win.loadFile('index.html')
+  win.loadFile(path.join(__dirname, './index.html'))
 
-  // win.close('closed', () => {
-  //   win = null
-  // })
+  win.webContents.openDevTools()
+  const template = [
+    {
+      label: 'Wallet',
+      submenu: [
+        {role: 'Create Account'},
+        {role: 'redo'}
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {role: 'reload'},
+        {role: 'forcereload'}
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click () { require('electron').shell.openExternal('https://electronjs.org') }
+        }
+      ]
+    }
+  ]
+
+  if (process.platform === 'darwin') {
+    template.unshift({
+      label: app.getName(),
+      submenu: [
+        {role: 'about'},
+        {type: 'separator'},
+        {role: 'services', submenu: []},
+        {type: 'separator'},
+        {role: 'hide'},
+        {role: 'hideothers'},
+        {role: 'unhide'},
+        {type: 'separator'},
+        {role: 'quit'}
+      ]
+    })
+
+    // Edit menu
+    template[1].submenu.push(
+      {type: 'separator'},
+      {
+        label: 'Speech',
+        submenu: [
+          {role: 'startspeaking'},
+          {role: 'stopspeaking'}
+        ]
+      }
+    )
+
+    // Window menu
+    template[3].submenu = [
+      {role: 'close'},
+      {role: 'minimize'},
+      {role: 'zoom'},
+      {type: 'separator'},
+      {role: 'front'}
+    ]
+  }
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+  win.on('closed', () => {
+    win = null
+  })
 }
 
 app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
