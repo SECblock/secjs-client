c371a8f39f57cf2d3d5731ce9da2ed6a6c4de217
theFay <fei.minhao@gmail.com>
2018-08-14 18:05:13 +0200

    begin with ui

--- webapp/src/renderer/App.vue
+++ webapp/src/renderer/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
+    <menu-header></menu-header>
     <router-view></router-view>
-  </div>
+  </div> -->
+  <v-app>
+    <v-container fluid>
+      <v-flex align-center>
+        <menu-header></menu-header>
+      </v-flex>
+    </v-container>
+  </v-app>  
 </template>
 
 <script>
-  export default {
-    name: 'webapp'
+import Vue from "vue";
+import Buefy from "buefy";
+import "buefy/lib/buefy.css";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Buefy);
+Vue.use(Vuetify, {
+  iconfont: "mdi"
+});
+
+import MenuHeader from "./components/MenuHeader";
+export default {
+  name: "webapp",
+  components: {
+    MenuHeader
   }
+}
 </script>
 
 <style>
-  /* CSS */
+/* CSS */
 </style>
